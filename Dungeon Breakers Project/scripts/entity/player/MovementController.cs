a9c0b8afeff7a2129504963f0e00e267cde4e710
Dawid Tkacz <104102596+Dawid2506@users.noreply.github.com>
2023-12-15 20:46:12 +0100

    clear MovementController code

--- Dungeon Breakers Project/scripts/entity/player/MovementController.cs
+++ Dungeon Breakers Project/scripts/entity/player/MovementController.cs
@@ -4,16 +4,9 @@ using game;
 using System.Threading.Tasks;
 public partial class MovementController : Node2D
 {
-    public float playerX { get; set; }
     Grid gridClass = Grid.Instance;
     StaticEntityList staticEntityList = StaticEntityList.Instance;
     private bool stop = false;
-
-    public override void _Ready()
-    {
-        playerX = this.GlobalPosition.X;
-        GD.Print(playerX);
-    }
     public MovementController()
     {
         SetProcessInput(true);
@@ -30,6 +23,7 @@ public partial class MovementController : Node2D
         HandleInputAction("playerE", "e");
         HandleInputAction("playerI", "i");
         HandleInputAction("playerMouseClick", "mouseClick");
+        HandleInputAction("playerM", ChangeScene);
     }
 
     private void HandleInputAction(string action, string message)
@@ -55,19 +49,14 @@ public partial class MovementController : Node2D
 
     private async void goRight()
     {
-        //GlobalPosition = new Vector2(GlobalPosition.X + 100, GlobalPosition.Y);
-        GD.Print("GlobalPosition");
+        //Coordinates of the grid cell to the right of the player
         int playerX = (int)(GlobalPosition.X + 50) / 100;
         int playerY = (int)GlobalPosition.Y / 100;
-        //GD.Print($"colision: {staticEntityList.Entities[gridClass.grid[playerX, playerY, 6]].interactable}");
-        GD.Print($"xcz: {gridClass.grid[11, 4, 6]}");
 
-        if ((gridClass.grid[playerX, playerY, 6] == 0 || (!staticEntityList.Entities[gridClass.grid[playerX, playerY, 6]].interactable)) && !stop)
+        if (CanMove(playerX, playerY))
         {
             GlobalPosition = new Vector2(GlobalPosition.X + 100, GlobalPosition.Y);
-            stop = true;
             await wait();
-            stop = false;
         }
     }
 
@@ -77,12 +66,10 @@ public partial class MovementController : Node2D
     {
         int playerX = (int)(GlobalPosition.X - 100) / 100;
         int playerY = (int)GlobalPosition.Y / 100;
-        if ((gridClass.grid[playerX, playerY, 6] == 0 || (!staticEntityList.Entities[gridClass.grid[playerX, playerY, 6]].interactable)) && !stop)
+        if (CanMove(playerX, playerY))
         {
             GlobalPosition = new Vector2(GlobalPosition.X - 100, GlobalPosition.Y);
-            stop = true;
             await wait();
-            stop = false;
         }
     }
 
@@ -90,12 +77,10 @@ public partial class MovementController : Node2D
     {
         int playerX = (int)GlobalPosition.X / 100;
         int playerY = (int)(GlobalPosition.Y + 50) / 100;
-        if ((gridClass.grid[playerX, playerY, 6] == 0 || (!staticEntityList.Entities[gridClass.grid[playerX, playerY, 6]].interactable)) && !stop)
+        if (CanMove(playerX, playerY))
         {
             GlobalPosition = new Vector2(GlobalPosition.X, GlobalPosition.Y + 100);
-            stop = true;
             await wait();
-            stop = false;
         }
     }
 
@@ -103,21 +88,27 @@ public partial class MovementController : Node2D
     {
         int playerX = (int)GlobalPosition.X / 100;
         int playerY = (int)(GlobalPosition.Y - 100) / 100;
-        if ((gridClass.grid[playerX, playerY, 6] == 0 || (!staticEntityList.Entities[gridClass.grid[playerX, playerY, 6]].interactable)) && !stop)
+        if (CanMove(playerX, playerY))
         {
             GlobalPosition = new Vector2(GlobalPosition.X, GlobalPosition.Y - 100);
-            stop = true;
             await wait();
-            stop = false;
         }
     }
+    // This method checks if the player can move to the given coordinates.
+    // It first checks if the grid cell at the player's target position is empty (grid value is 0).
+    // If the grid cell is not empty, it checks if the entity at that position is not interactable.
+    // It also checks if the 'stop' flag is not set.
+    // If all these conditions are met, the player can move, so the method returns true.
+    // Otherwise, it returns false.
+    private bool CanMove(int playerX, int playerY)
+{
+    return (gridClass.grid[playerX, playerY, 6] == 0 || (!staticEntityList.Entities[gridClass.grid[playerX, playerY, 6]].interactable)) && !stop;
+}
 
     public async Task wait()
     {
-        // Your code here
-        GD.Print("YourMethodName");
-        await ToSignal(GetTree().CreateTimer(0.1f), "timeout");
-        GD.Print("YourMethodNameeeee");
-        // More of your code here
+        stop = true;
+        await ToSignal(GetTree().CreateTimer(0.1f), "timeout"); // wait 0.1 seconds
+        stop = false;
     }
 }
